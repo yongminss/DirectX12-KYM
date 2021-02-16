11e097a9af5699c9c9a53710544780314713bc4e
Kim Yong Min <kyongmin96@gmail.com>
2021-02-16 13:35:16 +0900

    Update Scene
    리소스를 그래픽스 파이프라인에 연결하기 위해 루트 시그너쳐 생성

--- DirectX12_KYM_Project/MainGame/Scene.cpp
+++ DirectX12_KYM_Project/MainGame/Scene.cpp
@@ -15,5 +15,18 @@ Scene::~Scene()
 void Scene::CreateRootSignature(ID3D12Device &Device)
 {
 	D3D12_ROOT_SIGNATURE_DESC RootSignatureDesc;
+	RootSignatureDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT; // 입력-조립(IA) 단계 허용
+	RootSignatureDesc.pParameters = nullptr;
+	RootSignatureDesc.NumParameters = 0;
+	RootSignatureDesc.pStaticSamplers = nullptr;
+	RootSignatureDesc.NumStaticSamplers = 0;
 
+	ID3DBlob *RootSignatureBlob = nullptr;
+	ID3DBlob *ErrorBlob = nullptr;
+
+	D3D12SerializeRootSignature(&RootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, &RootSignatureBlob, &ErrorBlob);
+	Device.CreateRootSignature(0, RootSignatureBlob->GetBufferPointer(), RootSignatureBlob->GetBufferSize(), __uuidof(ID3D12RootSignature), (void**)&m_RootSignature);
+
+	if (RootSignatureBlob != nullptr) RootSignatureBlob->Release();
+	if (ErrorBlob != nullptr) ErrorBlob->Release();
 }
